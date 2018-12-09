bc4f74a384dd5b221929bb45ccfe5e30f7029dd9
Brent Humphrey <brent@snapspot.io>
2018-12-09 12:57:07 -0500

    Added a custom search bar renderer
    Added a custom search bar renderer to show how to a custom renderer can still be used with a ViewFactory.

---
+++ DependencyInjectionExamples/Renderers/CustomSearchBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DependencyInjectionExamples.Renderers
+{
+    public class CustomSearchBar : SearchBar
+    {
+        public CustomSearchBar()
+        {
+        }
+    }
+}
